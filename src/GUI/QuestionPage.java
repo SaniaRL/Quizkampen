c5917b428d092e0a1cf24bb464ab9c53ff5a8cdb
SaniaRL <saniarunnfors@gmail.com>
2023-11-19 20:15:36 +0100

    QuestionPage works, did some changes in QuestionCollections to make it safer.

--- src/GUI/QuestionPage.java
+++ src/GUI/QuestionPage.java
@@ -6,6 +6,9 @@ import Question.Question;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.io.IOException;
 
@@ -13,6 +16,8 @@ public class QuestionPage extends JPanel {
 
     QuestionCollection questionCollection;
     List<Question> questionList;
+    List<Question> threeQuestions;
+    String testCategory;
 
     JLabel questionLabel;
 
@@ -23,10 +28,14 @@ public class QuestionPage extends JPanel {
     String backgroundImagePath;
     Image backgroundImage;
 
+    int indexCount;
+
     public QuestionPage() throws IOException {
 
         questionCollection = new QuestionCollection();
         questionList = questionCollection.getAllQuestions();
+        threeQuestions = new ArrayList<>();
+        testCategory = "Musik";
 
         questionLabel = new JLabel();
 
@@ -37,6 +46,8 @@ public class QuestionPage extends JPanel {
         backgroundImagePath = "Backgrounds/blueBackground.png";
         backgroundImage = new ImageIcon(backgroundImagePath).getImage();
 
+        indexCount = 0;
+
         addComponents();
 
     }
@@ -46,11 +57,16 @@ public class QuestionPage extends JPanel {
         setLayout(new BorderLayout());
         setOpaque(true);
 
+        findThreeQuestion();
+
         generationNorthPanel();
         add(northPanel, BorderLayout.NORTH);
 
         generateCenterPanel();
         add(centerPanel, BorderLayout.CENTER);
+
+        generateSouthPanel();
+        add(southPanel, BorderLayout.SOUTH);
 
         setVisible(true);
     }
@@ -69,10 +85,10 @@ public class QuestionPage extends JPanel {
         centerPanel.setOpaque(false);
 
 
-        JLabel questionLabel = new JLabel("Får får får?", SwingConstants.CENTER);
+        JLabel questionLabel = new JLabel((threeQuestions.get(indexCount)).getQuestion(), SwingConstants.CENTER);
         questionLabel.setFont(new Font("Montserrat", Font.PLAIN, 22));
         questionLabel.setPreferredSize(new Dimension(500, 200));
-        questionLabel.setBackground(Color.lightGray);
+        questionLabel.setBackground(Color.white);
         questionLabel.setBorder(new LineBorder(Color.BLACK));
         questionLabel.setOpaque(true);
 
@@ -98,12 +114,35 @@ public class QuestionPage extends JPanel {
     }
 
     public void generateSouthPanel(){
-        northPanel.setLayout(new GridLayout(1,3));
-        northPanel.setPreferredSize(new Dimension(800, 300));
-        northPanel.setOpaque(false);
+        southPanel.setLayout(new GridLayout(2,2));
+        southPanel.setPreferredSize(new Dimension(800, 300));
+        southPanel.setOpaque(false);
 
+        List<String> optionsList = new ArrayList<>(Arrays.stream((threeQuestions.get(indexCount)).getQuestionOptions()).toList());
+        Collections.shuffle(optionsList);
+        if(optionsList.size() == 4){
+            for(int i = 0; i < optionsList.size(); i++){
+                JButton button = new JButton(optionsList.get(i));
+                button.setPreferredSize(new Dimension(200, 100));
+                button.setBackground(Color.white);
+//                button.setOpaque(true);
+                southPanel.add(button, SwingConstants.CENTER);
+            }
+        }
 
-        JLabel questionLabel = new JLabel();
+    }
+
+    public void findThreeQuestion(){
+            for(Question question : questionList){
+                if(threeQuestions.size() < 3){
+                    if(question.getCategory().label.equals(testCategory)){
+                        threeQuestions.add(question);
+                    }
+                }
+            }
+    }
+
+    public void generateRoundedBorder(){
 
     }
 
