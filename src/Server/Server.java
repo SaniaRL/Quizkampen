c098965c670ca7ca236b194d864b3854644e796a
Robin Blondin <robin.blondin@protonmail.com>
2023-11-20 09:53:38 +0100

    Added shutdown method

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,18 +2,50 @@ package Server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Server {
+    List<ClientHandler> connectedClients = Collections.synchronizedList(new ArrayList<>());
+    ServerSocket socket;
+    private boolean running = true;
+
     public Server(int port) {
 
-        try(ServerSocket socket = new ServerSocket(port)) {
-            while (true) {
-                new ServerListener(socket.accept()).start();
+        try {
+            socket = new ServerSocket(port);
+
+            while (running) {
+                Socket clientSocket = socket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket, this);
+                connectedClients.add(clientHandler);
+                clientHandler.start();
             }
         } catch (IOException e) {
             System.out.println("Server error");
         }
     }
+
+    public void shutdown() {
+        running = false;
+
+        try {
+            for (ClientHandler client : connectedClients) {
+                client.writeToClient("Server is shutting down");
+                client.closeConnection();
+            }
+
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error closing socket: " + e.getMessage());
+        }
+        connectedClients.clear();
+    }
+
     public static void main(String[] args) {
         int port = 8080;
         new Server(port);
