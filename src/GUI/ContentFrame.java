bab74629dba23038e53f9eb8106d7118400a9282
BurnedBySnow <sebastian.karlsson97@gmail.com>
2023-11-29 12:58:02 +0100

    removed import

--- src/GUI/ContentFrame.java
+++ src/GUI/ContentFrame.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.awt.*;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ContentFrame extends JFrame implements Serializable {
 
